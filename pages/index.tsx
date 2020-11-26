724331cedfbacd3534718d447f5fb24035416a79
chocopie116 <chocopie116@gmail.com>
2020-11-26 16:55:14 +0900

    impl

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,36 @@
 import React from 'react'
-export default () => <div>Hello world!!!</div>
+
+export default function Index({posts: posts}) {
+    return (
+        <>
+            <div>Hello world!!!</div>
+            {posts.map((p) => {
+                return (
+                    <div>
+                        <p>{ p.title }</p>
+                        <p>{ p.id }</p>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export async function getServerSideProps(context) {
+    return {
+      props: {
+          posts: [
+              {
+                  id: 'dfdsafdasfdsa',
+                  title: 'test',
+                  thumbnail: 'https://chocopie116.me/img/projects/zero9.jpg'
+              },
+              {
+                  id: 'dfdsafdasfdsa',
+                  title: 'test',
+                  thumbnail: 'https://chocopie116.me/img/projects/zero9.jpg'
+              }
+          ]
+      }
+    }
+}

294a58be94de0fa7050a76ecc75003193e8f7029
chocopie116 <chocopie116@gmail.com>
2020-11-27 22:34:45 +0900

    impl

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,111 @@ import React from "react";
 export default function Index({ posts: posts }) {
   return (
     <>
-      <div>Hello world!!!</div>
-      {posts.map((p) => {
-        return (
-          <div>
-            <p>{p.title}</p>
-            <p>{p.id}</p>
+      <div id="wrapper">
+        <header id="header" className="alt">
+          <a href="index.html" className="logo">
+            <strong>about me</strong>
+          </a>
+          <nav>
+            <a href="index.html#menu"></a>
+          </nav>
+        </header>
+
+        <nav id="menu">
+          <ul className="links">
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="index.html#projects">Projects</a>
+            </li>
+            <li>
+              <a href="https://note.mu/chocopie116/m/m603ca11b459e">Blogs</a>
+            </li>
+          </ul>
+        </nav>
+
+        <section id="banner" className="major">
+          <div className="inner">
+            <header className="major">
+              <h1>Work as Play</h1>
+            </header>
+            <div className="content">
+              <p>All work and no play makes Jack a dull boy.</p>
+              <ul className="actions">
+                <li>
+                  <a href="index.html#projects" className="button next scrolly">
+                    GET KNOWN
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        );
-      })}
+        </section>
+
+        <div id="main">
+          <div className="inner" id="projects">
+            <header className="major">
+              <h2>Projects</h2>
+            </header>
+          </div>
+
+          <section id="one" className="tiles">
+            <article
+              style={{ backgroundImage: `url(${"/img/projects/zero9.jpg"})` }}
+            >
+              <a href="https://chocopie116.me/projects/e-scooter/">
+                <span className="image">
+                  <img src="/img/projects/zero9.jpg" alt="" />
+                </span>
+                <header className="major">
+                  <h3>電動キックボードの社会実装</h3>
+                  <p>法律と向き合うこと・新しい文化をつくる挑戦</p>
+                </header>
+              </a>
+            </article>
+            <article
+              style={{
+                backgroundImage: `url(${"/img/projects/agriculture.jpg"})`,
+              }}
+            >
+              <a href="https://chocopie116.me/projects/e-scooter/">
+                <span className="image">
+                  <img src="/img/projects/zero9.jpg" alt="" />
+                </span>
+                <header className="major">
+                  <h3>電動キックボードの社会実装</h3>
+                  <p>法律と向き合うこと・新しい文化をつくる挑戦</p>
+                </header>
+              </a>
+            </article>
+          </section>
+
+          <section id="two">
+            <div className="inner">
+              <header className="major">
+                <h2>Contact</h2>
+              </header>
+              <p>
+                以下のいずれでも構いませんので気軽に連絡ください。何かよい出会いになると嬉しいです。
+              </p>
+              <ul>
+                <li>ページ記載のProjectに興味がある</li>
+                <li>
+                  Webシステム開発でメンバーが足りないので、仕事をお願いしたい
+                </li>
+                <li>
+                  新規事業を行うので幅広くやれるメンバーに最初の立ち上げを手伝ってほしい
+                </li>
+                <li>やりたいことがあるので一緒にやれる仲間が欲しい</li>
+              </ul>
+              <a href="https://m.me/chocopie116" target="_blank">
+                <img src="img/contactme.png" />
+              </a>
+            </div>
+          </section>
+        </div>
+      </div>
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  const res = await fetch("https://cocre.microcms.io/api/v1/works", {
-    headers: {
-      "X-API-KEY": "dab4d32d-1f4b-44f4-a76d-3020cb4f92fe",
-    },
-  });
-  const body = await res.json();
-  console.log(body);
-
-  return {
-    props: {
-      posts: [
-        {
-          id: "dfdsafdasfdsa",
-          title: "test",
-          thumbnail: "https://chocopie116.me/img/projects/zero9.jpg",
-        },
-        {
-          id: "dfdsafdasfdsa",
-          title: "test",
-          thumbnail: "https://chocopie116.me/img/projects/zero9.jpg",
-        },
-      ],
-    },
-  };
 }

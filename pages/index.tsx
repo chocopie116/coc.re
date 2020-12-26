77631a39ffbfaebc16bd23f4cdadab950f3c017f
chocopie116 <chocopie116@gmail.com>
2020-12-26 17:31:12 +0900

    fix warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,42 @@
-import React from "react";
+import React from 'react';
 
 export default function Index({ posts: posts }) {
   return (
     <>
-      <div id="wrapper">
-        <header id="header" className="alt">
-          <a href="index.html" className="logo">
+      <div id='wrapper'>
+        <header id='header' className='alt'>
+          <a href='index.html' className='logo'>
             <strong>株式会社コクリ</strong>
           </a>
           <nav>
-            <a href="index.html#menu"></a>
+            <a href='index.html#menu'></a>
           </nav>
         </header>
 
-        <nav id="menu">
-          <ul className="links">
+        <nav id='menu'>
+          <ul className='links'>
             <li>
-              <a href="index.html">Home</a>
+              <a href='index.html'>Home</a>
             </li>
             <li>
-              <a href="index.html#projects">Projects</a>
+              <a href='index.html#projects'>Projects</a>
             </li>
             <li>
-              <a href="https://note.mu/chocopie116/m/m603ca11b459e">Blogs</a>
+              <a href='https://note.mu/chocopie116/m/m603ca11b459e'>Blogs</a>
             </li>
           </ul>
         </nav>
 
-        <section id="banner" className="major">
-          <div className="inner">
-            <header className="major">
+        <section id='banner' className='major'>
+          <div className='inner'>
+            <header className='major'>
               <h1>Work as Play</h1>
             </header>
-            <div className="content">
+            <div className='content'>
               <p>All work and no play makes Jack a dull boy.</p>
-              <ul className="actions">
+              <ul className='actions'>
                 <li>
-                  <a href="index.html#projects" className="button next scrolly">
+                  <a href='index.html#projects' className='button next scrolly'>
                     GET KNOWN
                   </a>
                 </li>
@@ -45,23 +45,24 @@ export default function Index({ posts: posts }) {
           </div>
         </section>
 
-        <div id="main">
-          <div className="inner" id="projects">
-            <header className="major">
+        <div id='main'>
+          <div className='inner' id='projects'>
+            <header className='major'>
               <h2>Works</h2>
             </header>
           </div>
 
-          <section id="one" className="tiles">
+          <section id='one' className='tiles'>
             {posts.map((p) => {
               return (
                 <article
+                  key={p.id}
                   style={{
                     backgroundImage: `url(${p.thumbnail})`,
                   }}
                 >
-                  <a href="https://chocopie116.me/projects/e-scooter/">
-                    <header className="major">
+                  <a href='https://chocopie116.me/projects/e-scooter/'>
+                    <header className='major'>
                       <h3>{p.title}</h3>
                       <p>法律と向き合うこと・新しい文化をつくる挑戦</p>
                     </header>
@@ -71,14 +72,14 @@ export default function Index({ posts: posts }) {
             })}
           </section>
 
-          <section id="two">
-            <div className="inner">
-              <header className="major">
+          <section id='two'>
+            <div className='inner'>
+              <header className='major'>
                 <h2>Contact</h2>
               </header>
               <p>相談・雑談歓迎します。</p>
-              <a href="https://m.me/chocopie116" target="_blank">
-                <img src="img/contactme.png" />
+              <a href='https://m.me/chocopie116' target='_blank'>
+                <img src='img/contactme.png' />
               </a>
             </div>
           </section>
@@ -89,9 +90,9 @@ export default function Index({ posts: posts }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch("https://cocre.microcms.io/api/v1/works", {
+  const res = await fetch('https://cocre.microcms.io/api/v1/works', {
     headers: {
-      "X-API-KEY": "dab4d32d-1f4b-44f4-a76d-3020cb4f92fe",
+      'X-API-KEY': 'dab4d32d-1f4b-44f4-a76d-3020cb4f92fe',
     },
   });
   const body = await res.json();
@@ -101,14 +102,14 @@ export async function getServerSideProps(context) {
     props: {
       posts: [
         {
-          id: "dfdsafdasfdsa",
-          title: "test",
-          thumbnail: "https://chocopie116.me/img/projects/zero9.jpg",
+          id: 'dfdsafdasfdsa',
+          title: 'test',
+          thumbnail: 'https://chocopie116.me/img/projects/zero9.jpg',
         },
         {
-          id: "dfdsafdasfdsa",
-          title: "test",
-          thumbnail: "https://chocopie116.me/img/projects/zero9.jpg",
+          id: 'dfdsafdasfdsaa',
+          title: 'test',
+          thumbnail: 'https://chocopie116.me/img/projects/zero9.jpg',
         },
       ],
     },
